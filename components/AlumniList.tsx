7603f06c8586bcc166a471cc04223924b2aa765f
Miftah Syarief <miftahsyarief@Miftahs-MacBook-Pro.local>
2025-09-20 23:50:33 +0700

    refactoring

--- components/AlumniList.tsx
+++ components/AlumniList.tsx
@@ -184,14 +184,17 @@ export default function AlumniList() {
             {/** munculkan foto dari photoUrl di section ini. disebelah kiri list nama */}
             {a.photoUrl && (
               // <Image src={resolvePhotoUrl(a.photoUrl)!} alt={`Foto ${a.name}`} width={64} height={64} className="h-16 w-16 object-cover rounded-full mr-4 float-left" />
-              <img
-                src={resolvePhotoUrl(a.photoUrl)}
+              <Image
+                src={resolvePhotoUrl(a.photoUrl)!}
                 alt={`Foto ${a.name}`}
+                width={64}
+                height={64}
+                unoptimized
                 className="h-16 w-16 object-cover rounded-full mr-4 float-left"
-                onError={(e) => {
+                onError={(e: React.SyntheticEvent<HTMLImageElement>) => {
                   const img = e.currentTarget;
-                  if ((img as any)._triedFlip) return;       // cegah loop
-                  (img as any)._triedFlip = true;
+                  if (img.dataset.triedFlip === '1') return; // cegah loop
+                  img.dataset.triedFlip = '1';
                   img.src = flipExtCase(img.src);
                 }}
               />
@@ -232,14 +235,17 @@ export default function AlumniList() {
                   <div className="mr-4">
                     {selected.photoUrl ? (
                       //foto alumni tidak terlihat sepertinya butuh URL yang benar
-                      <img
-                        src={resolvePhotoUrl(selected.photoUrl)}
+                      <Image
+                        src={resolvePhotoUrl(selected.photoUrl)!}
                         alt={`Foto ${selected.name}`}
+                        width={80}
+                        height={80}
+                        unoptimized
                         className="h-20 w-20 object-cover rounded-md border"
-                        onError={(e) => {
+                        onError={(e: React.SyntheticEvent<HTMLImageElement>) => {
                           const img = e.currentTarget;
-                          if ((img as any)._triedFlip) return;
-                          (img as any)._triedFlip = true;
+                          if (img.dataset.triedFlip === '1') return;
+                          img.dataset.triedFlip = '1';
                           img.src = flipExtCase(img.src);
                         }}
                       />
