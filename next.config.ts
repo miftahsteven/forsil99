262d3064ebfae80f9b8d386f65afa50e3dafec45
Miftah Syarief <miftahsyarief@Miftahs-MacBook-Pro.local>
2025-09-20 15:50:37 +0700

    refactoring next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/profiles/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, proxy-revalidate' },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'Expires', value: '0' },
+        ],
+      },
+    ];
+  },
+  // Jika ingin paling simpel: nonaktifkan optimizer seluruhnya (opsional)
+  // images: { unoptimized: true },
 };
 
 export default nextConfig;
