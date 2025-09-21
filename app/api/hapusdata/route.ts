eaffeb6f8af1e0fcf8286c758240c5f0373c27f2
Miftah Syarief <miftahsyarief@Miftahs-MacBook-Pro.local>
2025-09-21 10:06:04 +0700

    refactoring hapus data

--- app/api/hapusdata/route.ts
+++ app/api/hapusdata/route.ts
@@ -4,13 +4,13 @@ import { getAuth, deleteUser } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 
 const app = initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DB_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 });
 const db = getDatabase(app);
 const auth = getAuth(app);
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
                     if (userRecord.exists()) {
                         const uid = userRecord.val().uid;
                         if (uid) {
-                            await deleteUser(uid); // pastikan currentUser sudah di-set dengan benar
+                            await deleteUser(auth.currentUser!); // pastikan auth.currentUser ada
                         }
                     }
                 } catch {
